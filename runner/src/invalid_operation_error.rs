aa1ea6f623b67f5bebee61ee6772ba4860e37d7f
Tokeiya <net_seed@hotmail.com>
2025-10-22 18:07:14 +0900

    Add `InvalidOperationError` and extend `BrandRecord` functionality
    - Introduced `InvalidOperationError` struct for operation-specific error handling.
    - Updated `BrandRecord::sharp_ratio` to return a `Result` with potential `InvalidOperationError`.
    - Added unit tests for `BrandRecord` methods to improve test coverage.
    - Included `invalid_operation_error` module in `main.rs` imports.

---
+++ runner/src/invalid_operation_error.rs
@@ -0,0 +1,11 @@
+use std::fmt::Display;
+use thiserror::Error;
+
+#[derive(Debug, Error)]
+pub struct InvalidOperationError(String);
+
+impl Display for InvalidOperationError {
+	fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+		write!(f, "InvalidOperation:{}", self.0)
+	}
+}
--- runner/src/investment/brand_record.rs
+++ runner/src/investment/brand_record.rs
@@ -1,4 +1,5 @@
 use crate::argument_error::ArgumentError;
+use crate::invalid_operation_error::InvalidOperationError;
 
 pub struct BrandRecord {
 	name: String,
@@ -23,7 +24,53 @@ impl BrandRecord {
 		todo!()
 	}
 
-	pub fn sharp_ratio(&self, rf: f64) -> f64 {
+	pub fn sharp_ratio(&self, rf: f64) -> Result<f64, InvalidOperationError> {
+		todo!()
+	}
+}
+
+#[cfg(test)]
+mod tests {
+	use super::*;
+
+	#[test]
+	fn new_test() {
+		let fixture = BrandRecord::new("test".to_string(), 0.25, 0.5).unwrap();
+		assert_eq!(fixture.name, "test");
+		assert_eq!(fixture.mean_interest, 0.25);
+		assert_eq!(fixture.deviation, 0.5);
+
+		let fixture = BrandRecord::new("test".to_string(), 0.25, 0.0).unwrap();
+		assert_eq!(fixture.name, "test");
+		assert_eq!(fixture.mean_interest, 0.25);
+		assert_eq!(fixture.deviation, 0.0);
+
+		let fixture = BrandRecord::new("test".to_string(), 0.0, -0.25)
+			.err()
+			.unwrap();
+		assert!(matches!(fixture, ArgumentError::ArgumentOutOfRange(_)));
+	}
+
+	#[test]
+	fn name_test() {
+		let fixture = BrandRecord::new("test".to_string(), 0.25, 0.5).unwrap();
+		assert_eq!(fixture.name(), "test");
+	}
+
+	#[test]
+	fn mean_interest_test() {
+		let fixture = BrandRecord::new("test".to_string(), 0.25, 0.5).unwrap();
+		assert_eq!(fixture.mean_interest(), 0.25);
+	}
+
+	#[test]
+	fn deviation_test() {
+		let fixture = BrandRecord::new("test".to_string(), 0.25, 0.5).unwrap();
+		assert_eq!(fixture.deviation(), 0.5);
+	}
+
+	#[test]
+	fn sharp_ratio_test() {
 		todo!()
 	}
 }
